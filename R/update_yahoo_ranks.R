cddc015f621f3ed5b44b334d7ab48a181ebca4ae
ctloftin <ctloftin@ncsu.edu>
2018-08-10 14:20:32 -0400

    Fixed column naming issue when reading existing ranking data, re-did match check

--- R/update_yahoo_ranks.R
+++ R/update_yahoo_ranks.R
@@ -1,5 +1,5 @@
 #' @export
-update_yahoo_ranks <- function(gameid = "380", leagueid = NULL, numPlayers = 200) {
+update_yahoo_ranks <- function(gameid = "380", leagueid = NULL, numPlayers = 300) {
   check_token()
 
   plist <- get_player_list(gameid, leagueid, numPlayers)
@@ -8,18 +8,22 @@ update_yahoo_ranks <- function(gameid = "380", leagueid = NULL, numPlayers = 200
   colnames(plist) <- c("Player",  "Team", "Pos", format(Sys.Date(), "%m/%d"))
 
   ranks <- read.csv(file = "data/yahoo_ranks.csv", header = TRUE, stringsAsFactors = FALSE)
+  colnames(ranks) <- gsub("X", "", colnames(ranks))
+  colnames(ranks) <- gsub("\\.", "/", colnames(ranks))
 
   ranks <- merge(ranks, plist, by = c("Player", "Team", "Pos"), all.x = TRUE)
   nonunique <- grep("X", colnames(ranks))
   if(length(nonunique) > 0) {
     ranks <- ranks[,-nonunique]
   }
+
   ranks <- ranks[order(ranks[,ncol(ranks)]),]
   ranks$match <- ranks[,ncol(ranks)-1] == ranks[,ncol(ranks)]
-  if(!(FALSE %in% ranks$match)) {
-    ranks[,ncol(ranks)-1] <- NULL
-  }
-  ranks$match <- NULL
 
-  write.csv(ranks, file = "data/yahoo_ranks.csv", row.names = FALSE)
+  if(FALSE %in% ranks$match) {
+    ranks$match <- NULL
+
+    write.csv(ranks, file = "data/yahoo_ranks.csv", row.names = FALSE)
+  }
+
 }
